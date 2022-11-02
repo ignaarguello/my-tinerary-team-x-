0f5004798fb4c659c158f1fc8b22483fe9e61c20
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2022-11-02 12:32:09 -0300

    datos de ciudades, usuarios e itinerarios

---
+++ src/users.js
@@ -0,0 +1,45 @@
+let users = [{
+        id: 'user1',
+        name: 'Lionel',
+        lastName: 'Messi',
+        age: '36',
+        email: 'lionelmessi@gmail.com',
+        password: 'thiagoymateo7',
+        code: 'papel-piedra-tijera',
+        verified: true,
+        logged: true
+    },
+    {
+        id: 'user2',
+        name: 'Rodrigo',
+        lastName: 'Palacio',
+        age: '37',
+        email: 'rodrigopalacio@gmail.com',
+        password: 'rodrigopalsd',
+        code: 'nutriapapelrio',
+        verified: true,
+        logged: true
+    },
+    {
+        id: 'user3',
+        name: 'Gonzalo',
+        lastName: 'Higuain',
+        age: '20',
+        email: 'pipitacrack@gmail.com',
+        password: 'hola1234',
+        code: 'bacnogalicia123',
+        verified: true,
+        logged: true
+    },
+    {
+        id: 'user4',
+        name: 'Emiliano',
+        lastName: 'Martinez',
+        age: '39',
+        email: 'eldibumartinez@gmail.com',
+        password: 'copaamerica21',
+        code: 'perrogatociencia',
+        verified: true,
+        logged: true
+    },
+]

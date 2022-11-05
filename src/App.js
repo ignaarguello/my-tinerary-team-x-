ca2c4e17cd8f1a3eee11b7afb74372a97b9b1758
Fabrizio Catanzaro <101287470+FabrizioCatanzaro@users.noreply.github.com>
2022-11-05 04:04:43 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,25 +5,20 @@ import { Component } from 'react';
 import './App.css';
 
 // importo las paginas de componentes
-import Home from './pages/Home'
-import SignUp from './pages/SignUp/SignUp'
-import Layout from './layout/Layout/Layout';
-
-
 
 function App() {
   return (
     <>
-      <Routes>
+      {/* <Routes> */}
         {/* <Route path="/cities" element={<Cities />}/> */}
         {/* <Route path="/hotels" element={<Hotels />}/> */}
-        <Route path="/index" element={<Home />}/>
+        {/* <Route path="/index" element={<Home />}/> */}
         {/* <Route path="/signin" element={<SignIn />}/> */}
-        <Route path="/signup" element={<SignUp />}/>
+        {/* <Route path="/signup" element={<SignUp />}/> */}
         {/* <Route path="/newcity" element={<NewCity />}/> */}
         {/* <Route path="/newhotel" element={<NewHotel />}/> */}
         {/* <Route path="/notfound" element={<NotFound />}/> */}
-      </Routes>
+      {/* </Routes> */}
     </>
   )
 }
